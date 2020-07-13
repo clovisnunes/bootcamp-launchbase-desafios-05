4bdce90b90ebf1f1a2983d3d2e076a7f08d87252
clovisnunes <clobnjuh@hotmail.com>
2020-07-13 19:46:15 -0300

    Mobile fixes

---
+++ models/script.sql
@@ -0,0 +1,10 @@
+CREATE TABLE TEST(
+    ID INT PRIMARY KEY,
+    avatar_url TEXT NOT NULL,
+    name TEXT NOT NULL,
+    birth_date TIMESTAMP NOT NULL,
+    education_level TEXT NOT NULL,
+    class_type TEXT NOT NULL,
+    subjects_taught TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL
+);
